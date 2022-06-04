72b226a85220212f16548daa681164556aa791e7
Nevin Allan Foster <78281215+NevinAF@users.noreply.github.com>
2022-06-04 08:29:31 -0700

    Update for time zone on not hosting

--- bot.js
+++ bot.js
@@ -56,7 +56,7 @@ const weekdays = [
 function attemptTime(str)
 {
 	let dayadder = -1;
-	let houraddr = 0;
+	let houraddr = -5;
 	weekdays.forEach(function(d, i)
 	{
 		//console.log(`${str} vs ${d}`, str.startsWith(d + " "), str.endsWith(" " + d));
